bd514324abca855ddd46b208a1713795a427a09b
Eoin Nolan <nolan.eoin@gmail.com>
2019-05-06 19:08:54 +0100

    image updates

--- source/details.html.jsx
+++ source/details.html.jsx
@@ -2,7 +2,7 @@ import Layout from "./layout.html.jsx"
 
 export default (props) => {
   let piece = props.data.pamalam.pieces.find(piece => piece.path === props.path);
-
+  piece.imagePath = piece.path.replace(/-/g, '_')
   return (
     <Layout title={`Pamalam | ${piece.title}`}>
       <div className="main-panel grey">
@@ -31,7 +31,7 @@ export default (props) => {
             </a>
           </div>
           <div class="layout__box o__flexes-to-1 o__has-columns o__centers-all">
-            <img className="hero-model" alt={piece.title} src={`/assets/images/pieces/${piece.path}/1.jpg`} />
+            <img className="hero-model" alt={piece.title} src={`/assets/images/pieces/${piece.path}/${piece.imagePath}1.jpg`} />
           </div>
         </div>
       </div>
