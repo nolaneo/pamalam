e4ff7ca1497c0ef187461c76c5d1200f5b11cc0f
Eoin Nolan <eoin.nolan@intercom.io>
2019-06-03 16:09:11 -0300

    site release

--- javascripts/index.js
+++ javascripts/index.js
@@ -12,9 +12,8 @@
       new Swiper(".swiper-container", {
         direction: "horizontal",
         loop: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev"
+        pagination: {
+          el: ".swiper-pagination"
         }
       });
     }
