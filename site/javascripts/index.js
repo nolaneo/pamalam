e9e51e381a85c1f3b7af79809c97f94e00fc8eaa
Eoin Nolan <eoin.nolan@intercom.io>
2019-06-08 13:20:54 -0300

    site release

--- site/javascripts/index.js
+++ site/javascripts/index.js
@@ -1,20 +1,20 @@
 (function () {
   'use strict';
 
-  document.addEventListener("scroll", function () {
-    if (!window.showingIntercom) {
-      window.showingIntercom = true;
-      setTimeout(function () {
-        Intercom("update", {
-          hide_default_launcher: false
-        });
-      }, 500);
-    }
-  });
+  // document.addEventListener("scroll", () => {
+  //   if (!window.showingIntercom) {
+  //     window.showingIntercom = true;
+  //     setTimeout(function() {
+  //       Intercom("update", {
+  //         hide_default_launcher: false
+  //       });
+  //     }, 500);
+  //   }
+  // });
   document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".chat-with-us").addEventListener("click", function () {
-      Intercom("showNewMessage");
-    });
+    // document.querySelector(".chat-with-us").addEventListener("click", function() {
+    //   Intercom("showNewMessage");
+    // });
     document.querySelectorAll(".details__thumbnail").forEach(function (thumbnail) {
       thumbnail.addEventListener("click", function (e) {
         document.querySelector(".details__main-image").style.backgroundImage = e.target.style.backgroundImage;
@@ -38,49 +38,45 @@
   }
 
   gtag("js", new Date());
-  gtag("config", "UA-141344238-1");
-  window.intercomSettings = {
-    app_id: "ec3q1hf4",
-    hide_default_launcher: true
-  };
+  gtag("config", "UA-141344238-1"); // window.intercomSettings = {
 
-  (function () {
-    var w = window;
-    var ic = w.Intercom;
+  !function () {
+    var t = window.driftt = window.drift = window.driftt || [];
 
-    if (typeof ic === "function") {
-      ic("reattach_activator");
-      ic("update", w.intercomSettings);
-    } else {
-      var d = document;
-
-      var i = function i() {
-        i.c(arguments);
+    if (!t.init) {
+      if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+      t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"], t.factory = function (e) {
+        return function () {
+          var n = Array.prototype.slice.call(arguments);
+          return n.unshift(e), t.push(n), t;
+        };
+      }, t.methods.forEach(function (e) {
+        t[e] = t.factory(e);
+      }), t.load = function (t) {
+        var e = 3e5,
+            n = Math.ceil(new Date() / e) * e,
+            o = document.createElement("script");
+        o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+        var i = document.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(o, i);
       };
-
-      i.q = [];
-
-      i.c = function (args) {
-        i.q.push(args);
-      };
-
-      w.Intercom = i;
-
-      var l = function l() {
-        var s = d.createElement("script");
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://widget.intercom.io/widget/ec3q1hf4";
-        var x = d.getElementsByTagName("script")[0];
-        x.parentNode.insertBefore(s, x);
-      };
-
-      if (w.attachEvent) {
-        w.attachEvent("onload", l);
-      } else {
-        w.addEventListener("load", l, false);
-      }
     }
-  })();
+  }();
+  drift.config({
+    enableWelcomeMessage: false
+  });
+  drift.on("ready", function (api) {
+    api.widget.hide();
+    document.addEventListener("scroll", function () {
+      if (!window.showingDrift) {
+        window.showingDrift = true;
+        setTimeout(function () {
+          api.widget.show();
+        }, 500);
+      }
+    });
+  });
+  drift.SNIPPET_VERSION = "0.3.1";
+  drift.load("62n6fp4hvuex");
 
 }());
