926959910e8dfe9631063eb54b88dc618a37109e
Eoin Nolan <eoin.nolan@intercom.io>
2019-07-04 18:13:00 -0300

    Add Pams nerd docs

---
+++ source/privacy-policy.html.jsx
@@ -0,0 +1,247 @@
+import Layout from "./layout.html.jsx";
+
+export default props => {
+  return (
+    <Layout title="Pamalam | Privacy Policy">
+      <div class="boring-page">
+        <h1>PRIVACY POLICY</h1>
+
+        <p>
+          If you are a visitor to a Pamalam website, event or trade show
+          exhibition, a recipient of Pamalam communications or a customer of
+          Pamalam, then except as expressly set forth below, this Privacy Policy
+          applies to you.
+        </p>
+
+        <h2>WHAT INFORMATION DO WE COLLECT</h2>
+
+        <p>
+          Pamalam is a Data Controller for the personal data collected on
+          www.pamalam.com, at Pamalam events, stores or trade shows.
+        </p>
+
+        <p>
+          {" "}
+          When you purchase an item from this site, we collect necessary
+          personal information such as your name, address and email address for
+          the purpose of fulfilling the order.
+        </p>
+
+        <p>
+          When you browse our site, we automatically receive your computer’s
+          internet protocol (IP) address in order to provide us with information
+          that helps us learn about your browser and operating system.
+        </p>
+
+        <p>
+          If you attend a Pamalam event, store or trade show exhibition we may
+          collect data such as your name, address and email address for the
+          purpose of sending you marketing information.
+        </p>
+
+        <h2>LEGAL BASIS FOR PROCESSING</h2>
+
+        <p>Performance of a Contract:</p>
+
+        <p>
+          When a transaction is completed on this website a contract is created
+          between Pamalam and the buyer (you). In order to complete the contract
+          we will process the personal data as provided.
+        </p>
+
+        <p>Consent and legitimate interest:</p>
+
+        <p>
+          If we ask for your personal information for marketing or other
+          purposes, we will ask you directly for your expressed consent.
+        </p>
+
+        <p>
+          Pamalam is a provider of premium clothing, in order to engage
+          customers and potential customers we have a legitimate interest to
+          collect relevant data and send messages where we, in good faith,
+          believe the recipient could be interested in hearing from us. We will
+          always include an opportunity to object or unsubscribe.
+        </p>
+
+        <p>How do I withdraw my consent or object to processing?</p>
+
+        <p>
+          If after you opt-in, you change your mind, you may withdraw your
+          consent for us to contact you, for the continued collection, use or
+          disclosure of your information, at anytime, by contacting us at
+          privacy@pamalam.com.
+        </p>
+
+        <h2>SHARING YOUR INFORMATION WITH THIRD PARTIES</h2>
+
+        <p>
+          We will never sell, trade, share your personal information with third
+          parties except in the following limited circumstances:
+        </p>
+        <p>
+          We use a small number of third party processors that help us with our
+          business activities. These include but aren’t limited to: Shopify, for
+          processing payments and Intercom for customer messaging. All third
+          parties act as data processors and are engaged under contract and
+          obliged to meet appropriate security requirements and comply with all
+          applicable legislation.
+        </p>
+        <p>
+          We may share your personal information when we have a good faith
+          belief that disclosure of such information is reasonably necessary to
+          satisfy any applicable law, regulation, legal process or enforceable
+          governmental request or enforce a contract, including investigation of
+          potential violations thereof, or protect against imminent harm to our
+          rights, property or safety, or that of our users or the public as
+          required or permitted by law;
+        </p>
+        <p>
+          We may share your personal information with third parties (including
+          our service providers and government entities) to detect, prevent, or
+          otherwise address fraud or security or technical issues;
+        </p>
+        <p>
+          We may share and/or transfer your personal information if we become
+          involved in a merger, acquisition, bankruptcy, or any form of sale of
+          some or all of our assets; and
+        </p>
+        <p>
+          We may share your personal information with a third party if we have
+          your consent to do so.
+        </p>
+
+        <h2>LINKS</h2>
+        <p>
+          If you click on a link on our site, it is possible that it may direct
+          you away from our site. We are not responsible for the privacy
+          practices of other sites and encourage you to read their privacy
+          statements.
+        </p>
+
+        <h2>SECURITY</h2>
+
+        <p>
+          To protect your personal information, we take reasonable precautions
+          and follow industry best practices to make sure it is not
+          inappropriately lost, misused, accessed, disclosed, altered or
+          destroyed.
+        </p>
+
+        <h2>AGE OF CONSENT</h2>
+
+        <p>
+          Pamalam is not intended for use by anyone under the age of 16, nor
+          does Pamalam knowingly collect or solicit personal information from
+          anyone under the age of 16. If you are under 16, you may not attempt
+          to purchase or send any information about yourself to us, including
+          your name, address, telephone number, or email address. In the event
+          that we confirm that we have collected personal information from
+          someone under the age of 16 without verification of parental consent,
+          we will delete that information promptly. If you have any questions or
+          concerns about this please contact us at privacy@pamalam.com
+        </p>
+
+        <h2>CHANGES TO THIS PRIVACY POLICY</h2>
+
+        <p>
+          We reserve the right to modify this privacy policy at any time.
+          Changes and clarifications will take effect immediately upon their
+          posting on the website. If we make material changes to this policy, we
+          will notify you here that it has been updated, so that you are aware
+          of what information we collect, how we use it, and under what
+          circumstances, if any, we use and/or disclose it.
+        </p>
+
+        <h2> YOUR RIGHTS UNDER GDPR</h2>
+
+        <p>
+          Pamalam would like to make sure you are fully aware of all of your
+          data protection rights.
+        </p>
+
+        <p>Every user is entitled to the following:</p>
+
+        <p>
+          The right to access – You have the right to request Pamalam provide
+          you with copies of your personal data. We may charge you a small fee
+          for this service.
+        </p>
+
+        <p>
+          The right to rectification – You have the right to request that
+          Pamalam correct any information you believe is inaccurate. You also
+          have the right to request Pamalam to complete the information you
+          believe is incomplete.
+        </p>
+
+        <p>
+          The right to erasure – You have the right to request that Pamalam
+          erase your personal data, under certain conditions.
+        </p>
+
+        <p>
+          The right to restrict processing – You have the right to request that
+          Pamalam restrict the processing of your personal data, under certain
+          conditions.
+        </p>
+
+        <p>
+          The right to object to processing – You have the right to object to
+          Pamalam’s processing of your personal data, under certain conditions.
+        </p>
+
+        <p>
+          The right to data portability – You have the right to request that
+          Pamalam transfer the data that we have collected to another
+          organization, or directly to you, under certain conditions.
+        </p>
+
+        <p>
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us at
+          privacy@pamalam.com. If you are unsatisfied with the response you have
+          the right to lodge a complaint with your supervisory authority.
+        </p>
+
+        <h2>COOKIES</h2>
+
+        <p>
+          Cookies are text files placed on your computer to collect standard
+          Internet log information and visitor behavior information. When you
+          visit our website, we may collect information from you automatically
+          through cookies or similar technology
+        </p>
+        <p>For further information, visit allaboutcookies.org.</p>
+        <p>How do we use cookies?</p>
+
+        <p>
+          Pamalam uses cookies in a range of ways to improve your experience on
+          our website, including:
+        </p>
+
+        <p>
+          So that we recognize you on our website and remember your previously
+          selected preferences or items placed in your cart.
+        </p>
+        <p>Understanding how you use our website</p>
+        <p>To remember conversations sent through our Messenger.</p>
+        <p>How to manage cookies</p>
+
+        <p>
+          You can set your browser to not accept cookies, and the above website
+          tells you how to remove cookies from your browser. However, in a few
+          cases, some of our website features may not function as a result.
+        </p>
+
+        <h2>QUESTIONS AND CONTACT INFORMATION</h2>
+
+        <p>
+          If you would like to: access, correct, amend or delete any personal
+          information we have about you, register a complaint, or simply want
+          more information contact Please contact us at privacy@pamalam.com.
+        </p>
+      </div>
+    </Layout>
+  );
+};
